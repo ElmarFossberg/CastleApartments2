802b53b9bcf1401281fb27bac085b14e1f846847
ElmarFossberg <Elmarrunarsson@gmail.com>
2025-05-13 21:03:54 +0000

    ye

--- CastleApartments2/settings.py
+++ CastleApartments2/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apartment.apps.ApartmentConfig'
+    'apartment.apps.ApartmentConfig',
+    'user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
--- user/views.py
+++ user/views.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        print(1)
+    else:
+        return render(request, "user/register.html", {
+            'form': UserCreationForm()
+        })
